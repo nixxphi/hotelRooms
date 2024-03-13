01bf468337acd3f87d85718f1824782debdc79d2
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-13 09:29:54 +0100

    Create roomTypeSchema.js

---
+++ roomTypeSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const roomTypeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+});
+
+const RoomType = mongoose.model('RoomType', roomTypeSchema);
+
+module.exports = RoomType;
