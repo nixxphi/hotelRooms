4a9d58d023fdc2c12ba127a78c5d480e7de85e8b
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-13 14:08:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 // Importing necessary modules
+const app = express();
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 const roomTypeRouter = require('./roomTypeRouter');
+const roomRouter = require('./roomRouter');
 const errorHandler = require('./errorHandler');
 
+// Load environment variables from .env file
+dotenv.config();
+
+// Create Express app
 const app = express();
 
+// Middleware
+app.use(bodyParser.json());
 // MongoDB connection
 mongoose.connect('mongodb+srv://nixxphi:this.is.the@redcluster.pixh5su.mongodb.net/?retryWrites=true&w=majority&appName=Redcluster', {
   useNewUrlParser: true,
